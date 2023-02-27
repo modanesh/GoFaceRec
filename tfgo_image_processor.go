a69fc31e3d7b0a29a3ca8b19b66689ade00f1c2a
modanesh <mohamad4danesh@gmail.com>
2023-02-27 16:50:12 +0330

    minor updates

--- tfgo_image_processor.go
+++ tfgo_image_processor.go
@@ -7,29 +7,35 @@ import (
 	tg "github.com/galeone/tfgo"
 	"github.com/galeone/tfgo/image"
 	"os"
+	"strconv"
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) < 6 {
 		fmt.Println("How to run:\n\tfind-circles [imgfile]")
 		return
 	}
 	filename := os.Args[1]
+	imgSize := os.Args[2]
+	modelPath := os.Args[3]
+	inputLayer := os.Args[4]
+	outputLayer := os.Args[5]
+	intImgSize, _ := strconv.ParseFloat(imgSize, 32)
 
 	root := tg.NewRoot()
 	img := image.Read(root, filename, 3)
-	img = img.ResizeArea(image.Size{Height: 112, Width: 112})
-	r_img := op.Reshape(root, img.Value(), tg.Const(root, [3]int32{3, 112, 112}))
-	r_img = op.ExpandDims(root, r_img, tg.Const(root, []int32{0}))
-	results := tg.Exec(root, []tf.Output{r_img}, nil, &tf.SessionOptions{})
+	img = img.ResizeArea(image.Size{Height: float32(intImgSize), Width: float32(intImgSize)})
+	rImg := op.Reshape(root, img.Value(), tg.Const(root, [3]int32{3, int32(intImgSize), int32(intImgSize)}))
+	rImg = op.ExpandDims(root, rImg, tg.Const(root, []int32{0}))
+	results := tg.Exec(root, []tf.Output{rImg}, nil, &tf.SessionOptions{})
 	tensor_img, _ := tf.NewTensor(results[0].Value())
 
-	qmf_model := tg.LoadModel("./magface_epoch_00025_pb", []string{"serve"}, nil)
+	qmf_model := tg.LoadModel(modelPath, []string{"serve"}, nil)
 
 	qmf_results := qmf_model.Exec([]tf.Output{
-		qmf_model.Op("PartitionedCall", 0),
+		qmf_model.Op(inputLayer, 0),
 	}, map[tf.Output]*tf.Tensor{
-		qmf_model.Op("serving_default_input.1", 0): tensor_img,
+		qmf_model.Op(outputLayer, 0): tensor_img,
 	})
 
 	qmf_predictions := qmf_results[0]
